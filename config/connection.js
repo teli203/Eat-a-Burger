867fc7d51efea7f002dfbd34ad81d924f9e81e45
Telicia Watson <teli203@aol.com>
2021-01-09 13:27:06 -0600

    connection set up from previous folder starting over

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,27 @@
-var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+// Set up MySQL connection //
+const mysql = require("mysql");
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "hardworkeR/1",
+        database: "eat_a_burger"
+    });
+}
+
+// Make connection //
+connection.connect((err) => {
+    if (err) {
+        console.error(`Error connecting to DB: ${err.stack}`);
+        return;
+    }
+    console.log(`Connected to DB with ID ${connection.threadId}`);
 });
 
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+// Export connection for our ORM to use.
 module.exports = connection;
