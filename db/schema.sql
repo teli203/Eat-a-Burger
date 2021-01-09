e76a895bc982c192afca6a0688e82fe7be0a3026
Telicia Watson <teli203@aol.com>
2021-01-09 13:52:52 -0600

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,10 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
-USE burgers_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
--- If the table already exists, remove it before trying to create the table again
-DROP TABLE IF EXISTS burgers;
-
--- Create the burgers table
-CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
-    devoured BOOL DEFAULT false,
-    PRIMARY KEY (id)
+CREATE TABLE burgers
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
 );
