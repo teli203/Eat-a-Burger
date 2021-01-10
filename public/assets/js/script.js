3f694fb18e44280b7a0ed92a13bae7115097980a
Telicia Watson <teli203@aol.com>
2021-01-09 21:05:20 -0600

    adding the staust change, & pg reload

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,17 +1,57 @@
-$(document).ready(function() {
-    
-  $(".devour-form").on("submit", function(event) {
-    event.preventDefault();
+$(function () {
+  $(".change-status").on("click", function (event) {
+      const id = $(this).data("id");
+      const newStatus = $(this).data("newstatus");
 
-    var burger_id = $(this).children(".burger_id").val();
-    console.log(burger_id);
-    $.ajax({
-      method: "PUT",
-      url: "/burgers/" + burger_id
-    }).then(function(data) {
-      // reload page to display devoured burger in proper column
-      location.reload();
-    });
+      const newStatusState = {
+          eaten: newStatus
+      };
 
+      // Send the PUT request //
+      $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newStatusState
+      }).then(
+          function () {
+              console.log("changed status to", newStatus);
+              // Reload the page to get the updated list //
+              location.reload();
+          }
+      );
+  });
+
+  $(".create-form").on("submit", function (event) {
+      event.preventDefault();
+
+      const newBurger = {
+          name: $("#burger").val().trim(),
+      };
+
+      // Send the POST request //
+      $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+      }).then(
+          function () {
+              console.log("created new burger:", newBurger);
+              
+              location.reload();
+          }
+      );
+  });
+
+  $(".delete-burger").on("click", function (event) {
+      const id = $(this).data("id");
+
+      // Send the DELETE request //
+      $.ajax("/api/burgers/" + id, {
+          type: "DELETE"
+      }).then(
+          function () {
+              console.log("deleted burger", id);
+              
+              location.reload();
+          }
+      );
   });
 });
